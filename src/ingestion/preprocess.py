e439c52636e66b7b2f43d4e8845d3f21876a89f5
calderkatyal <katyalcalder@gmail.com>
2025-03-20 18:41:40 +0530

    Finish dataloader.py and begin preprocess.py

--- src/ingestion/preprocess.py
+++ src/ingestion/preprocess.py
@@ -1,13 +1,13 @@
-from ingestion.dataloader import load, save
+from src.ingestion.dataloader import load, save
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+RAW_CSV_PATH = os.path.join(BASE_DIR, "data", "raw", "emails.csv")
+PROCESSED_PATH = os.path.join(BASE_DIR, "data", "processed", "emails.parquet")
 
 def preprocess_emails(input_path, output_path):
-    print("Loading dataset...")
     df = load(input_path)
-    print("Saving preprocessed emails...")
     save(df, output_path)
 
 if __name__ == "__main__":
-    RAW_CSV_PATH = "data/raw/enron_emails.csv"
-    PROCESSED_PATH = "data/processed/cleaned_emails.parquet"
-
     preprocess_emails(RAW_CSV_PATH, PROCESSED_PATH)
