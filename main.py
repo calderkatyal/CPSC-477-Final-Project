bd545b3bdc5b69ac89f025c34d81cb26f3719098
calderkatyal <katyalcalder@gmail.com>
2025-04-20 18:34:00 -0400

    abstract into main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
+import subprocess
+from src.config import DATA_DIR, PROCESSED_DIR
 from src.hybrid_search.hybrid_search import run_search_interface
 
 if __name__ == "__main__":
+    if not os.path.isdir(DATA_DIR):
+        subprocess.run(["python", "-m", "src.scripts.download_data"], check=True)
+    if not os.path.isdir(PROCESSED_DIR):
+        subprocess.run(["python", "-m", "src.preprocessing.preprocess"], check=True)
+
     run_search_interface()
